27666f102846faff768928060126bcbcc84cdd5d
VladMallory <vladmallory0321@gmail.com>
2026-05-18 14:09:42 +1000

    первый коммит. nvim, zsh

---
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      options = {
+        opt = {
+          number = true,
+          relativenumber = false,
+          scrolloff = 5,
+          wrap = true,
+          linebreak = true,
+          breakindent = true,
+          breakindentopt = "shift:0,sbr",
+          showbreak = "↪  ",
+        },
+      },
+    },
+  },
+}
